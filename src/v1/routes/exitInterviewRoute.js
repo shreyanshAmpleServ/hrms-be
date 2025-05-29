14eecaefb90c78a5e0d50042e004bbe6971523cb
unknown <shivang.rawat@ampleserv.com>
2025-05-29 14:12:54 +0530

    Add exit interview feature with CRUD operations and routing

---
+++ src/v1/routes/exitInterviewRoute.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const exitInterview = require("../controller/exitInterviewController.js");
+const { authenticateToken } = require("../middlewares/authMiddleware.js");
+
+// Create exit interview routes
+router.post(
+  "/exit-interview",
+  authenticateToken,
+  exitInterview.createExitInterview
+);
+
+// Get all exit interviews routes
+router.get(
+  "/exit-interview",
+  authenticateToken,
+  exitInterview.getAllExitInterviews
+);
+
+// Get a single exit interview by ID routes
+router.get(
+  "/exit-interview/:id",
+  authenticateToken,
+  exitInterview.findExitInterview
+);
+
+// Update a exit interview by ID routes
+router.put(
+  "/exit-interview/:id",
+  authenticateToken,
+  exitInterview.updateExitInterview
+);
+
+// Delete  exit interview by ID routes
+router.delete(
+  "/exit-interview/:id",
+  authenticateToken,
+  exitInterview.deleteExitInterview
+);
+
+module.exports = router;
