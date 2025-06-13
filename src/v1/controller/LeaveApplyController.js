29d5b995ef24f2d865975454916b3a0b496a07f0
unknown <shivang.rawat@ampleserv.com>
2025-06-13 12:49:11 +0530

    Enhance attendance summary retrieval by adding date range filtering for employee attendance

--- src/v1/controller/LeaveApplyController.js
+++ src/v1/controller/LeaveApplyController.js
@@ -1,66 +1,124 @@
-const LeaveApplyService = require('../services/LeaveApplyService');
-const CustomError = require('../../utils/CustomError');
-const moment = require('moment');
+const LeaveApplyService = require("../services/LeaveApplyService");
+const CustomError = require("../../utils/CustomError");
+const moment = require("moment");
+const {
+  uploadToBackblaze,
+  deleteFromBackblaze,
+} = require("../../utils/uploadBackblaze");
+const fs = require("fs");
 
 const createLeaveApplication = async (req, res, next) => {
-    try {
-        const data = {
-            ...req.body,
-            createdby: req.user.id,
-            log_inst: req.user.log_inst,  }
-        const reqData = await LeaveApplyService.createLeaveApplication(data);
-        res.status(201).success('Leave application created successfully', reqData);
-    } catch (error) {
-        next(error);
+  try {
+    let imageUrl = null;
+
+    if (req.file) {
+      const fileBuffer = fs.readFileSync(req.file.path); // read file from disk
+      imageUrl = await uploadToBackblaze(
+        fileBuffer,
+        req.file.originalname,
+        req.file.mimetype,
+        "document_attachment"
+      );
+      fs.unlinkSync(req.file.path); // optional: delete file after upload
     }
+
+    const data = {
+      ...req.body,
+      createdby: req.user.id,
+      document_attachment: imageUrl,
+      log_inst: req.user.log_inst,
+    };
+
+    const reqData = await LeaveApplyService.createLeaveApplication(data);
+    res.status(201).success("Leave application created successfully", reqData);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const findLeaveApplicationById = async (req, res, next) => {
-    try {
-        const reqData = await LeaveApplyService.findLeaveApplicationById(req.params.id);
-        if (!reqData) throw new CustomError('Leave application not found', 404);
-        res.status(200).success(null, reqData);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const reqData = await LeaveApplyService.findLeaveApplicationById(
+      req.params.id
+    );
+    if (!reqData) throw new CustomError("Leave application not found", 404);
+    res.status(200).success(null, reqData);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const updateLeaveApplication = async (req, res, next) => {
-    try {
-        const data = {
-            ...req.body,
-            updatedby: req.user.id,
-            log_inst: req.user.log_inst,  }
-        const reqData = await LeaveApplyService.updateLeaveApplication(req.params.id, data);
-        res.status(200).success('Leave application updated successfully', reqData);
-    } catch (error) {
-        next(error);
+  try {
+    const existingData = await LeaveApplyService.findLeaveApplicationById(
+      req.params.id
+    );
+    if (!existingData)
+      throw new CustomError("Leave Application not found", 404);
+
+    let imageUrl = existingData.document_attachment;
+
+    if (req.file) {
+      const fileBuffer = fs.readFileSync(req.file.path);
+      imageUrl = await uploadToBackblaze(
+        fileBuffer,
+        req.file.originalname,
+        req.file.mimetype,
+        "document_attachment"
+      );
+      fs.unlinkSync(req.file.path); // optional: remove file after upload
+
+      if (existingData.document_attachment) {
+        await deleteFromBackblaze(existingData.document_attachment);
+      }
     }
+
+    const data = {
+      ...req.body,
+      document_attachment: imageUrl,
+      updatedby: req.user.id,
+      log_inst: req.user.log_inst,
+    };
+
+    const reqData = await LeaveApplyService.updateLeaveApplication(
+      req.params.id,
+      data
+    );
+    res.status(200).success("Leave application updated successfully", reqData);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const deleteLeaveApplication = async (req, res, next) => {
-    try {
-        await LeaveApplyService.deleteLeaveApplication(req.params.id);
-        res.status(200).success('Leave application deleted successfully', null);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await LeaveApplyService.deleteLeaveApplication(req.params.id);
+    res.status(200).success("Leave application deleted successfully", null);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getAllLeaveApplication = async (req, res, next) => {
-    try {
-            const { page , size ,search ,startDate,endDate  } = req.query;
-        const data = await LeaveApplyService.getAllLeaveApplication(search,Number(page), Number(size),startDate && moment(startDate),endDate && moment(endDate));
-        res.status(200).success(null, data);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { page, size, search, startDate, endDate } = req.query;
+    const data = await LeaveApplyService.getAllLeaveApplication(
+      search,
+      Number(page),
+      Number(size),
+      startDate && moment(startDate),
+      endDate && moment(endDate)
+    );
+    res.status(200).success(null, data);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    createLeaveApplication,
-    findLeaveApplicationById,
-    updateLeaveApplication,
-    deleteLeaveApplication,
-    getAllLeaveApplication,
+  createLeaveApplication,
+  findLeaveApplicationById,
+  updateLeaveApplication,
+  deleteLeaveApplication,
+  getAllLeaveApplication,
 };
--- src/v1/models/LeaveApplyModel.js
+++ src/v1/models/LeaveApplyModel.js
@@ -1,28 +1,37 @@
 const { PrismaClient } = require("@prisma/client");
 const CustomError = require("../../utils/CustomError");
-const { errorNotExist } = require("../../Comman/errorNotExist");
-const { leaveApplicationSchema } = require("../schemas/leaveApplicationSchema");
+const { id } = require("zod/v4/locales");
 const prisma = new PrismaClient();
 
-const serializeJobData = (data) => {
-  return {
-    employee_id: Number(data.employee_id) || null,
-    leave_type_id: Number(data.leave_type_id) || null,
-    start_date: data.start_date ? new Date(data.start_date) : new Date(),
-    end_date: data.end_date ? new Date(data.end_date) : new Date(),
-    reason: data.reason || "",
-    status: data.status || "",
-  };
-};
+// Serialize leave application data
+const serializeLeaveApplicationData = (data) => ({
+  employee_id: data.employee_id ? Number(data.employee_id) : null,
+  leave_type_id: data.leave_type_id ? Number(data.leave_type_id) : null,
+  start_date: data.start_date ? new Date(data.start_date) : null,
+  end_date: data.end_date ? new Date(data.end_date) : null,
+  reason: data.reason || "",
+  status: data.status || "",
+  partial_day: data.partial_day || "",
+  start_session: data.start_session || "",
+  end_session: data.end_session || "",
+  backup_person_id: data.backup_person_id
+    ? Number(data.backup_person_id)
+    : null,
+  contact_details_during_leave: data.contact_details_during_leave || "",
+  approver_id: data.approver_id ? Number(data.approver_id) : null,
+  approval_date: data.approval_date ? new Date(data.approval_date) : null,
+  document_attachment: data.document_attachment || "",
+  rejection_reason: data.rejection_reason || "",
+});
 
 // Create a new leave application
 const createLeaveApplication = async (data) => {
   try {
-    const validatedData = leaveApplicationSchema.parse(data);
-    await errorNotExist("hrms_d_employee", data.employee_id, "Employee");
+    console.log("Saving document_attachment:", data.document_attachment);
+
     const reqData = await prisma.hrms_d_leave_application.create({
       data: {
-        ...serializeJobData(data),
+        ...serializeLeaveApplicationData(data),
         createdby: data.createdby || 1,
         createdate: new Date(),
         log_inst: data.log_inst || 1,
@@ -40,6 +49,21 @@ const createLeaveApplication = async (data) => {
             id: true,
           },
         },
+        leave_approver: {
+          select: {
+            id: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        leave_backup_person_id: {
+          select: {
+            id: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        leave_types: true,
       },
     });
     return reqData;
@@ -51,14 +75,14 @@ const createLeaveApplication = async (data) => {
   }
 };
 
-// Find a leave application by ID
+// Find leave application by ID
 const findLeaveApplicationById = async (id) => {
   try {
     const reqData = await prisma.hrms_d_leave_application.findUnique({
       where: { id: parseInt(id) },
     });
     if (!reqData) {
-      throw new CustomError("leave application not found", 404);
+      throw new CustomError("Leave application not found", 404);
     }
     return reqData;
   } catch (error) {
@@ -69,107 +93,17 @@ const findLeaveApplicationById = async (id) => {
   }
 };
 
-// Update a leave application
+// Update leave application
 const updateLeaveApplication = async (id, data) => {
   try {
-    const validatedData = leaveApplicationSchema.parse(data);
-
-    await errorNotExist("hrms_d_employee", data.employee_id, "Employee");
-
-    const updatedLeaveApplication =
-      await prisma.hrms_d_leave_application.update({
-        where: { id: parseInt(id) },
-        data: {
-          ...serializeJobData(data),
-          updatedby: data.updatedby || 1,
-          updatedate: new Date(),
-        },
-        include: {
-          leave_employee: {
-            select: {
-              full_name: true,
-              id: true,
-            },
-          },
-          leave_types: {
-            select: {
-              leave_type: true,
-              id: true,
-            },
-          },
-        },
-      });
-    return updatedLeaveApplication;
-  } catch (error) {
-    throw new CustomError(
-      `Error updating leave application: ${error.message}`,
-      500
-    );
-  }
-};
-
-// Delete a leave application
-const deleteLeaveApplication = async (id) => {
-  try {
-    await prisma.hrms_d_leave_application.delete({
+    const updatedEntry = await prisma.hrms_d_leave_application.update({
       where: { id: parseInt(id) },
-    });
-  } catch (error) {
-    throw new CustomError(
-      `Error deleting leave application: ${error.message}`,
-      500
-    );
-  }
-};
 
-// Get all leave applications
-const getAllLeaveApplication = async (
-  search,
-  page,
-  size,
-  startDate,
-  endDate
-) => {
-  try {
-    page = !page || page == 0 ? 1 : page;
-    size = size || 10;
-    const skip = (page - 1) * size || 0;
-
-    const filters = {};
-    // Handle search
-    if (search) {
-      filters.OR = [
-        {
-          leave_employee: {
-            full_name: { contains: search.toLowerCase() },
-          },
-        },
-        {
-          leave_types: {
-            leave_type: { contains: search.toLowerCase() },
-          },
-        },
-        {
-          status: { contains: search.toLowerCase() },
-        },
-      ];
-    }
-
-    if (startDate && endDate) {
-      const start = new Date(startDate);
-      const end = new Date(endDate);
-
-      if (!isNaN(start.getTime()) && !isNaN(end.getTime())) {
-        filters.createdate = {
-          gte: start,
-          lte: end,
-        };
-      }
-    }
-    const datas = await prisma.hrms_d_leave_application.findMany({
-      where: filters,
-      skip: skip,
-      take: size,
+      data: {
+        ...serializeLeaveApplicationData(data),
+        updatedby: data.updatedby || 1,
+        updatedate: new Date(),
+      },
       include: {
         leave_employee: {
           select: {
@@ -183,10 +117,123 @@ const getAllLeaveApplication = async (
             id: true,
           },
         },
+        leave_approver: {
+          select: {
+            id: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        leave_backup_person_id: {
+          select: {
+            id: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        leave_types: true,
       },
-      orderBy: [{ updatedate: "desc" }, { createdate: "desc" }],
     });
-    // const totalCount = await prisma.hrms_d_leave_application.count();
+    return updatedEntry;
+  } catch (error) {
+    throw new CustomError(
+      `Error updating leave application: ${error.message}`,
+      500
+    );
+  }
+};
+
+// Delete leave application
+const deleteLeaveApplication = async (id) => {
+  try {
+    await prisma.hrms_d_leave_application.delete({
+      where: { id: parseInt(id) },
+    });
+  } catch (error) {
+    throw new CustomError(
+      `Error deleting leave application: ${error.message}`,
+      500
+    );
+  }
+};
+
+// Get all leave applications with pagination and search
+const getAllLeaveApplication = async (
+  search,
+  page,
+  size,
+  startDate,
+  endDate
+) => {
+  try {
+    page = !page || page == 0 ? 1 : page;
+    size = size || 10;
+    const skip = (page - 1) * size || 0;
+
+    const filters = {};
+    if (search) {
+      filters.OR = [
+        {
+          leave_approver: { full_name: { contains: search.toLowerCase() } },
+        },
+        {
+          leave_backup_person_id: {
+            full_name: { contains: search.toLowerCase() },
+          },
+        },
+        { reason: { contains: search.toLowerCase() } },
+        { status: { contains: search.toLowerCase() } },
+        { rejection_reason: { contains: search.toLowerCase() } },
+        {
+          contact_details_during_leave: {
+            contains: search.toLowerCase(),
+          },
+        },
+      ];
+    }
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      if (!isNaN(start.getTime()) && !isNaN(end.getTime())) {
+        filters.start_date = { gte: start, lte: end };
+      }
+    }
+
+    const datas = await prisma.hrms_d_leave_application.findMany({
+      where: filters,
+      skip,
+      take: size,
+      orderBy: [{ updatedate: "desc" }, { createdate: "desc" }],
+      include: {
+        leave_employee: {
+          select: {
+            full_name: true,
+            id: true,
+          },
+        },
+        leave_types: {
+          select: {
+            leave_type: true,
+            id: true,
+          },
+        },
+        leave_approver: {
+          select: {
+            id: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        leave_backup_person_id: {
+          select: {
+            id: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        leave_types: true,
+      },
+    });
     const totalCount = await prisma.hrms_d_leave_application.count({
       where: filters,
     });
@@ -196,7 +243,7 @@ const getAllLeaveApplication = async (
       currentPage: page,
       size,
       totalPages: Math.ceil(totalCount / size),
-      totalCount: totalCount,
+      totalCount,
     };
   } catch (error) {
     throw new CustomError("Error retrieving leave applications", 503);
