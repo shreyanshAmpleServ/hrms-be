c2ea51d35b3423d57cfa8014b090dca1f0784f2c
unknown <shivang.rawat@ampleserv.com>
2025-06-26 12:38:18 +0530

    Added hrLetterRoute and updated hrms_m_letter_type model with hr_letter_letter_type relation

---
+++ src/v1/models/hrLetterModel.js
@@ -0,0 +1,231 @@
+const { PrismaClient } = require("@prisma/client");
+const CustomError = require("../../utils/CustomError");
+const prisma = new PrismaClient();
+
+// Serialize incoming data
+const serializeHRLetter = (data) => ({
+  employee_id: data.employee_id ? Number(data.employee_id) : null,
+  letter_type: data.letter_type ? Number(data.letter_type) : null,
+  request_date: data.request_date ? new Date(data.request_date) : null,
+  issue_date: data.issue_date ? new Date(data.issue_date) : null,
+  document_path: data.document_path || "",
+  letter_subject: data.letter_subject || null,
+  letter_content: data.letter_content || null,
+  status: data.status || "Pending",
+});
+
+//  Create HR Letter
+const createhrLetter = async (data) => {
+  try {
+    const result = await prisma.hrms_d_hr_letter.create({
+      data: {
+        ...serializeHRLetter(data),
+        createdby: data.createdby || 1,
+        createdate: new Date(),
+        log_inst: data.log_inst || 1,
+      },
+      include: {
+        hrms_d_employee: {
+          select: {
+            id: true,
+            full_name: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        hr_letter_letter_type: {
+          select: { id: true, letter_name: true },
+        },
+      },
+    });
+    return result;
+  } catch (error) {
+    throw new CustomError(`Error creating HR letter: ${error.message}`, 500);
+  }
+};
+
+// Get by ID
+const gethrLetterById = async (id) => {
+  try {
+    const result = await prisma.hrms_d_hr_letter.findUnique({
+      where: { id: parseInt(id) },
+      include: {
+        hrms_d_employee: {
+          select: {
+            id: true,
+            full_name: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        hr_letter_letter_type: {
+          select: { id: true, letter_name: true },
+        },
+      },
+    });
+    if (!result) throw new CustomError("HR Letter not found", 404);
+    return result;
+  } catch (error) {
+    throw new CustomError(`Error fetching HR letter: ${error.message}`, 500);
+  }
+};
+
+// Update HR letter
+const updatehrLetter = async (id, data) => {
+  try {
+    const result = await prisma.hrms_d_hr_letter.update({
+      where: { id: parseInt(id) },
+      data: {
+        ...serializeHRLetter(data),
+        updatedby: data.updatedby || 1,
+        updatedate: new Date(),
+      },
+      include: {
+        hrms_d_employee: {
+          select: {
+            id: true,
+            full_name: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        hr_letter_letter_type: {
+          select: { id: true, letter_name: true },
+        },
+      },
+    });
+    return result;
+  } catch (error) {
+    throw new CustomError(`Error updating HR letter: ${error.message}`, 500);
+  }
+};
+
+//  Delete HR Letter
+const deletehrLetter = async (id) => {
+  try {
+    await prisma.hrms_d_hr_letter.delete({
+      where: { id: parseInt(id) },
+    });
+    return { message: "Deleted successfully" };
+  } catch (error) {
+    throw new CustomError(`Error deleting HR letter: ${error.message}`, 500);
+  }
+};
+
+//Get All HR Letters
+const getAllhrLetter = async (search, page, size, startDate, endDate) => {
+  try {
+    page = !page || page <= 0 ? 1 : page;
+    size = size || 10;
+    const skip = (page - 1) * size;
+
+    const filters = {};
+
+    if (search) {
+      filters.OR = [
+        { letter_subject: { contains: search.toLowerCase() } },
+        { status: { contains: search.toLowerCase() } },
+        {
+          hrms_d_employee: {
+            full_name: { contains: search.toLowerCase() },
+          },
+        },
+        {
+          hr_letter_letter_type: {
+            letter_type_name: { contains: search.toLowerCase() },
+          },
+        },
+      ];
+    }
+
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      filters.request_date = { gte: start, lte: end };
+    }
+
+    const results = await prisma.hrms_d_hr_letter.findMany({
+      where: filters,
+      skip,
+      take: size,
+      orderBy: [{ updatedate: "desc" }, { createdate: "desc" }],
+      include: {
+        hrms_d_employee: {
+          select: {
+            id: true,
+            full_name: true,
+            employee_code: true,
+            full_name: true,
+          },
+        },
+        hr_letter_letter_type: {
+          select: { id: true, letter_name: true },
+        },
+      },
+    });
+
+    const totalCount = await prisma.hrms_d_hr_letter.count({ where: filters });
+
+    return {
+      data: results,
+      currentPage: page,
+      size,
+      totalPages: Math.ceil(totalCount / size),
+      totalCount,
+    };
+  } catch (error) {
+    throw new CustomError(`Error fetching HR letters: ${error.message}`, 503);
+  }
+};
+
+const updatehrLetterStatus = async () => {
+  try {
+    const hrLetterId = parseInt(id);
+    console.log("Hr letter ID: ", hrLetterId);
+
+    if (isNaN(hrLetterId)) {
+      throw new CustomError("Invalid Hr letter ID", 400);
+    }
+
+    const existingCandidateMaster = await prisma.hrms_d_hr_letter.findUnique({
+      where: { id: hrLetterId },
+    });
+
+    if (!existingCandidateMaster) {
+      throw new CustomError(`Hr Letter with ID ${hrLetterId} not found`, 404);
+    }
+
+    const updateData = {
+      status: data.status,
+      updatedby: data.updatedby || 1,
+      updatedate: new Date(),
+    };
+    if (data.status === "Approved") {
+      updateData.status_remarks = data.status_remarks || "";
+    } else if (data.status === "Rejected") {
+      updateData.status_remarks = data.status_remarks || "";
+    } else {
+      updateData.status_remarks = "";
+    }
+    const updatedEntry = await prisma.hrms_d_hr_letter.update({
+      where: { id: hrLetterId },
+      data: updateData,
+    });
+
+    return updatedEntry;
+  } catch (error) {
+    throw new CustomError(
+      `Error updating Hr letter status: ${error.message}`,
+      500
+    );
+  }
+};
+
+module.exports = {
+  createhrLetter,
+  gethrLetterById,
+  updatehrLetter,
+  deletehrLetter,
+  getAllhrLetter,
+  updatehrLetterStatus,
+};
---
+++ src/v1/services/hrLetterService.js
@@ -0,0 +1,40 @@
+const hrLetterModel = require("../models/hrLetterModel.js");
+
+const createhrLetter = async (data) => {
+  return await hrLetterModel.createhrLetter(data);
+};
+
+const gethrLetterById = async (id) => {
+  return await hrLetterModel.gethrLetterById(id);
+};
+
+const updatehrLetter = async (id, data) => {
+  return await hrLetterModel.updatehrLetter(id, data);
+};
+
+const deletehrLetter = async (id) => {
+  return await hrLetterModel.deletehrLetter(id);
+};
+
+const updatehrLetterStatus = async (id, data) => {
+  return await hrLetterModel.updatehrLetterStatus(id, data);
+};
+
+const getAllhrLetter = async (search, page, size, startDate, endDate) => {
+  return await hrLetterModel.getAllhrLetter(
+    search,
+    page,
+    size,
+    startDate,
+    endDate
+  );
+};
+
+module.exports = {
+  createhrLetter,
+  gethrLetterById,
+  updatehrLetter,
+  deletehrLetter,
+  updatehrLetterStatus,
+  getAllhrLetter,
+};
