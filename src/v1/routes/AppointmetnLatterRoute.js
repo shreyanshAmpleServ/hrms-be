c6e6e27a2758abdbeabf7da98c44126bc1963187
Mani Kant Sharma <manikant.sharma@ampleserv.com>
2025-09-09 12:10:19 +0530

    mkx

--- src/v1/routes/AppointmetnLatterRoute.js
+++ src/v1/routes/AppointmetnLatterRoute.js
@@ -1,13 +1,59 @@
-const express = require('express');
-const AppointmentLatterController = require('../controller/AppointmentLatterController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
-
+const express = require("express");
+const AppointmentLatterController = require("../controller/AppointmentLatterController");
+const { authenticateToken } = require("../middlewares/authMiddleware");
+const {
+  setupNotificationMiddleware,
+} = require("../middlewares/notificationMiddleware");
 const router = express.Router();
 
-router.post('/appointment-letter', authenticateToken, AppointmentLatterController.createAppointmentLatter);
-router.get('/appointment-letter/:id', authenticateToken, AppointmentLatterController.findAppointmentLatterById);
-router.put('/appointment-letter/:id', authenticateToken, AppointmentLatterController.updateAppointmentLatter);
-router.delete('/appointment-letter/:id', authenticateToken, AppointmentLatterController.deleteAppointmentLatter);
-router.get('/appointment-letter', authenticateToken, AppointmentLatterController.getAllAppointmentLatter);
+router.post(
+  "/appointment-letter",
+  authenticateToken,
+  (req, res, next) =>
+    setupNotificationMiddleware(
+      req,
+      res,
+      next,
+      "Appointment Letters",
+      "create"
+    ),
+  AppointmentLatterController.createAppointmentLatter
+);
+router.get(
+  "/appointment-letter/:id",
+  authenticateToken,
+  AppointmentLatterController.findAppointmentLatterById
+);
+router.put(
+  "/appointment-letter/:id",
+  authenticateToken,
+  (req, res, next) =>
+    setupNotificationMiddleware(
+      req,
+      res,
+      next,
+      "Appointment Letters",
+      "update"
+    ),
+  AppointmentLatterController.updateAppointmentLatter
+);
+router.delete(
+  "/appointment-letter/:id",
+  authenticateToken,
+  (req, res, next) =>
+    setupNotificationMiddleware(
+      req,
+      res,
+      next,
+      "Appointment Letters",
+      "delete"
+    ),
+  AppointmentLatterController.deleteAppointmentLatter
+);
+router.get(
+  "/appointment-letter",
+  authenticateToken,
+  AppointmentLatterController.getAllAppointmentLatter
+);
 
 module.exports = router;
