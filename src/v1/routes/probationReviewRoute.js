73d0339f8d5e2e4177925c68308648a26f97ab9d
shivrawat05 <shivrawat2002@gmail.com>
2025-10-13 11:01:39 +0530

    change

--- src/v1/routes/probationReviewRoute.js
+++ src/v1/routes/probationReviewRoute.js
@@ -2,46 +2,34 @@ const express = require("express");
 const router = express.Router();
 const probationReviewController = require("../controller/probationReviewController.js");
 const { authenticateToken } = require("../middlewares/authMiddleware.js");
-const {
-  setupNotificationMiddleware,
-} = require("../middlewares/notificationMiddleware.js");
 
-// Create probation review  routes
 router.post(
   "/probation-review",
   authenticateToken,
   probationReviewController.createProbationReview
 );
 
-// Get all probation reviews routes
 router.get(
   "/probation-review",
   authenticateToken,
   probationReviewController.getAllProbationReview
 );
 
-// Get a single probation review by ID routes
 router.get(
   "/probation-review/:id",
   authenticateToken,
   probationReviewController.findProbationReview
 );
 
-// Update a probation review by ID routes
 router.put(
   "/probation-review/:id",
-  (req, res, next) =>
-    // setupNotificationMiddleware(req, res, next, "Probation Review", "update"),
-    authenticateToken,
+  authenticateToken,
   probationReviewController.updateProbationReview
 );
 
-// Delete  probation review by ID routes
 router.delete(
   "/probation-review/:id",
-  (req, res, next) =>
-    // setupNotificationMiddleware(req, res, next, "Probation Review", "delete"),
-    authenticateToken,
+  authenticateToken,
   probationReviewController.deleteProbationReview
 );
 
