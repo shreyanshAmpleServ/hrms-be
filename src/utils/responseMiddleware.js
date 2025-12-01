d3e65c7d740e4cf9581887d88e15997d2642b274
shivrawat05 <shivrawat2002@gmail.com>
2025-12-01 17:21:11 +0530

    Add checks to skip invalid and duplicate component codes during update - Alter payroll processing table to add missing columns dynamically

--- src/utils/responseMiddleware.js
+++ src/utils/responseMiddleware.js
@@ -1,20 +1,42 @@
+// const responseHandler = (req, res, next) => {
+//   res.success = (message, data) => {
+//     res.status(res.statusCode || 200).json({
+//       success: true,
+//       data: data,
+//       message: message || "Success",
+//       status: res.statusCode,
+//     });
+//   };
+//   res.error = (message, statusCode = 400) => {
+//     res.status(statusCode).json({
+//       success: false,
+//       data: null,
+//       message: message || "Error",
+//       status: statusCode,
+//     });
+//   };
+//   next();
+// };
+
 const responseHandler = (req, res, next) => {
   res.success = (message, data) => {
-    res.status(res.statusCode || 200).json({
+    return res.json({
       success: true,
       data: data,
       message: message || "Success",
-      status: res.statusCode,
+      status: res.statusCode || 200,
     });
   };
+
   res.error = (message, statusCode = 400) => {
-    res.status(statusCode).json({
+    return res.status(statusCode).json({
       success: false,
       data: null,
       message: message || "Error",
       status: statusCode,
     });
   };
+
   next();
 };
 
